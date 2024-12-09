0089ca9ba163fdca05e270d687917c93d9740eda
Luís Fernando Montes <montes.luis@totvs.com.br>
2024-12-09 15:13:00 -0300

    Alterar refatorando as pastas do projeto

--- galeria/views.py
+++ galeria/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 def index(request):
     if not request.user.is_authenticated:
         messages.error(request, 'Usuário não logado')
-        return redirect(request, 'login')
+        return redirect('login')
     
     fotografias = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
     return render(request, 'galeria/index.html',{'cards': fotografias})
